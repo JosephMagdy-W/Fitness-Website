26941117b26072c2b8b6b955328a8f3058e710a4
JOSEPH MAGDY HABIB <120096459+JosephMagdy-W@users.noreply.github.com>
2023-05-30 12:17:56 +0300

    Add files via upload
    html /CSS /JavaScript / jQuery

---
+++ js.js
@@ -0,0 +1,31 @@
+let promptMsg = prompt  ("What would you like to exercise today","Write a training today");
+
+console.log(promptMsg);
+
+alert("Welcome to Be Fit Gym  ");
+
+ // Get the login form element
+
+ function login(loginForm)
+{
+   if(loginForm.username.value && loginForm.password.value)
+
+{
+   var username=document.getElementById("username").value;
+   document.write('<html><body><h1><center>')
+       document.write("welcome"+" ");
+       document.write(username);
+       document.write('</center></h1></body></html>'); 
+}
+else
+alert("please enter your Username &password");
+}
+
+
+
+  
+$(document).ready(function(){
+    alert("we hope you a nice day ,jq ");
+ 
+ });
+  
